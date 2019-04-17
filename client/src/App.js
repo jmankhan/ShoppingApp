0aa76049f42c5bcf61987e13564b9130d6770deb
Matthew Hoare <matthoare013@gmail.com>
2019-04-17 12:25:21 -0400

    title bar

--- client/src/App.js
+++ client/src/App.js
@@ -4,20 +4,23 @@ import ShoppingMap from './components/ShoppingMap/ShoppingMap'
 import Search from './components/Search/Search'
 import ResultsList from './components/ResultsList/ResultsList'
 import {search} from './Api'
+import Typography from "@material-ui/core/Typography";
+import Toolbar from "@material-ui/core/Toolbar";
+import AppBar from "@material-ui/core/AppBar";
 
 const App = () => {
-    const [term, setTerm] = useState('')
-    const [results, setResults] = useState([])
+    const [term, setTerm] = useState('');
+    const [results, setResults] = useState([]);
 
     const onSearch = searchTerm => {
-        setTerm(searchTerm)
+        setTerm(searchTerm);
 
         //do api callout here
         search(searchTerm).then(response => {
             const items = response.items || [];
             const stores = response.stores || [];
             let itemsInStore = response.itemsInStore.filter(i => i.inStore != null) || [];
-            
+
             const result = {};
             //construct a data object that will store all items per store
             stores.forEach(store => {
@@ -25,35 +28,43 @@ const App = () => {
                 result[store.no]['items'] = itemsInStore
                     .filter(i => i.inStore.storeId === store.no)
                     .map(i => {
-                        const item = items.find(item => item.upc === i.common.productId.upca)
+                        const item = items.find(item => item.upc === i.common.productId.upca);
                         return {
                             ...item,
                             price: i.inStore.price.priceInCents,
                             quantity: i.inStore.inventory.quantity,
                             available: i.inStore.inventory.available
                         }
-                    })
-                    
+                    });
+
                 result[store.no]['store'] = store;
-            })
+            });
 
             setResults(result);
         })
-        
+
         //mock data
         // setResults([
         //     {name: 'Xbox One', price: 299.00, lat: 40.2087106, lng: -76.8707534},
         //     {name: 'Xbox One S', price: 399.00, lat: 40.2087106, lng: -76.8707534}
         // ])
-    }
+    };
 
+    // Yea yea I know this is bad ... but is due at 6
     return (
         <>
-            {!term && <Search search={onSearch} center={true} />}
-            {term && <ResultsList results={results} term={term} onSearch={onSearch} />}
-            {term && <ShoppingMap data={results} searchTerm={term} />}
+            <AppBar position="static" style={{background: '#9c27b0'}}>
+                <Toolbar>
+                    <Typography variant="h4" color="inherit">
+                        Shopping App
+                    </Typography>
+                </Toolbar>
+            </AppBar>
+            {!term && <Search search={onSearch} center={true}/>}
+            {term && <ResultsList results={results} term={term} onSearch={onSearch}/>}
+            {term && <ShoppingMap data={results} searchTerm={term}/>}
         </>
     )
-}
+};
 
 export default App;
